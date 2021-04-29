7cb7879674e8347e4923e5535ce411294c969167
chaim-ha <hayeschaim4@gmail.com>
2021-04-28 20:26:51 -0400

    added subtraction operation to the calculator

--- python_calc.py
+++ python_calc.py
@@ -4,7 +4,15 @@ calculator written in python
 by chaim hayes
 """
 
-a = int(input("Enter number: "))
-b = int(input("Enter number: "))
+userInput = input("For Addition: 1\nFor Subtraction: 2\nChoice: ")
 
-print(a+b)
+if userInput == "1":
+    a = int(input("Enter number: "))
+    b = int(input("Enter number: "))
+    print(a+b)
+elif userInput == "2":
+    a = int(input("Enter number: "))
+    b = int(input("Enter number: "))
+    print(a-b)
+else:
+    print("Invalid Input")
