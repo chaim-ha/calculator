c771ee768926a5863cd02399fb21eedc5e6a6487
chaim-ha <hayeschaim4@gmail.com>
2021-04-28 20:34:40 -0400

    While loop commented in

--- python_calc.py
+++ python_calc.py
@@ -3,6 +3,9 @@ Python calculator
 calculator written in python
 by chaim hayes
 """
+#Open ended while loop for testing code, "CTRL+C" to exit
+#inf=1
+#while inf==1:
 
 userInput = input("For Addition: 1\nFor Subtraction: 2\nChoice: ")
 
